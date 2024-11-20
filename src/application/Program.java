7ecdbf36fb1b2d03cf40c7e0a9da0b304ad407aa
Frederico Silva <fredericochopin@gmail.com>
2024-11-20 14:58:34 -0300

    Entity Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Literature");
-        System.out.println(department);
+        Seller seller = new Seller(12,
+                "Martin Fox",
+                "martin.fox@mylibrary.com",
+                new Date(),
+                4985.52,
+                department);
+        System.out.println(seller);
     }
 }
